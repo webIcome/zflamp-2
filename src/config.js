26ba6b62024c2913aa2333502220d071dada82bc
huangdaoguo <daoguo.huang@zifisense.co.uk>
2018-06-05 09:52:13 +0800

    mo

--- src/config.js
+++ src/config.js
@@ -2,14 +2,15 @@
  * Created by spring on 2018/4/8.
  */
 export default {
-    URL_API: 'http://192.168.0.12:20001/',
+    // URL_API: 'http://192.168.0.12:20001/',
     // URL_API: 'http://192.168.0.134:20001/',
-    // URL_API: 'http://www.zeta-alliance.com:20001', //正式网统一认证平台
+     URL_API: 'http://www.zeta-alliance.com:20001', //正式网统一认证平台
     // LAMP_URL_API: 'http://192.168.0.115:20003/',
-    LAMP_URL_API: 'http://192.168.0.152:20003/',
-    // LAMP_URL_API: 'http://www.zeta-alliance.com:20003',//正式网路灯后台地址
-    WELL_URL_API: 'http://192.168.0.152:20004/',
+    // LAMP_URL_API: 'http://192.168.0.152:20003/',
+     LAMP_URL_API: 'http://www.zeta-alliance.com:20003',//正式网路灯后台地址
+    // WELL_URL_API: 'http://192.168.0.152:20004/',
     // WELL_URL_API: 'http://192.168.0.115:20004/',
+    WELL_URL_API: 'http://www.zeta-alliance.com:20004/',
     HOME_PAGE_URL: '/',
     DEFAULT_PAGE_SIZE: 10,
 }
